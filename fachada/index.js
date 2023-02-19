15db1a91b044497ea44a4f48cd0908066b44774b
ueliot <ueliot2001@yahoo.com>
2023-02-19 03:25:49 +0100

    ejemplo final

--- fachada/index.js
+++ fachada/index.js
@@ -1,28 +1,7 @@
-/* =======Patron Fachada==========
-el objetivo de este patron es evitar leer un manual complejo de instrucciones
-o de como funciona una api, mas bien a simple vista que se pueda ver 
-de forma sencilla lo hace, otro objetivo es reducir el acoplamiento
-y seguir la ley demeter (deberiaos acceder directamente a clases conocidas)
 
-ejemplo: tengo un libreria de logs con una gran complejidad
-para usarla tengo que chequear permisos, si existe chekar si existe el folder
-crearle un nombre y al final escribir el mensaje
-=> creare una fachada para usarla de manera sencialla.
+import { AdvanceLogsLibrary } from "./AdvancedLogLib.js";
+import LogsFacade from "./logsfacade.js";
 
-*/
-//Configuracion para codespace
-//https://docs.github.com/en/codespaces/setting-up-your-project-for-codespaces/adding-a-dev-container-configuration/setting-up-your-nodejs-project-for-codespaces
-//https://www.youtube.com/watch?v=jRtrDpzN_5o
-//https://lenguajejs.com/automatizadores/vite/guia-tutorial-inicial-de-vite/
-//https://www.youtube.com/watch?v=NrXfZyvhGjA
-//https://vitejs.dev/guide/
-//https://github.com/vitejs/awesome-vite#templates
-import { AdvanceLogsLibrary } from "./AdvancedLogLib";
+let logsFacade = new LogsFacade(new AdvanceLogsLibrary());
 
-let logs = new AdvanceLogsLibrary();
-
-//esta logica podria complicarse mucho si hacemos uso de una api
-//muy extensa
-if (logs.checkPermission() && logs.checkExistFolder){
-    logs.write(logs.generaLogName());
-}
+logsFacade.write("HOLA FELIX");
