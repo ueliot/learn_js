2a4c53035e610dc621423698dae648a4e49ce193
ueliot <ueliot2001@yahoo.com>
2023-02-19 03:06:26 +0100

    añadido comentario

--- fachada/AdvancedLogLib.js
+++ fachada/AdvancedLogLib.js
@@ -1,15 +1,4 @@
-/* =======Patron Fachada==========
-el objetivo de este patron es evitar leer un manual complejo de instrucciones
-o de como funciona una api, mas bien a simple vista que se pueda ver 
-de forma sencilla lo hace, otro objetivo es reducir el acoplamiento
-y seguir la ley demeter (deberiaos acceder directamente a clases conocidas)
 
-ejemplo: tengo un libreria de logs con una gran complejidad
-para usarla tengo que chequear permisos, si existe chekar si existe el folder
-crearle un nombre y al final escribir el mensaje
-=> creare una fachada para usarla de manera sencialla.
-
-*/
 
 class AdvanceLogsLibrary {
     constructor(){
